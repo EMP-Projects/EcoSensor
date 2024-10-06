9ff44558364ac7004b84bf012877dcd787f913c3
gzileni <giuseppe.zileni@gmail.com>
2024-10-06 18:36:03 +0200

    fix and upgrade

--- backend/AirQuality/Properties/AirQualityPropertiesMapper.cs
+++ backend/AirQuality/Properties/AirQualityPropertiesMapper.cs
@@ -16,12 +16,6 @@ public class AirQualityPropertiesMapper : AbstractMapperProfile<AirQualityProper
             .ForMember(dest => dest.SourceText, opt => opt.MapFrom(src => Pollution.GetPollutionSource(src.Source)))
             .ForMember(dest => dest.PollutionText,
                 opt => opt.MapFrom(src => Pollution.GetPollutionDescription(src.Pollution)))
-            .ForMember(dest => dest.Gis, opt => opt.Ignore())
-            .ForMember(dest => dest.Lat, opt => opt.Ignore())
-            .ForMember(dest => dest.Lng, opt => opt.Ignore())
-            .ForMember(dest => dest.EntityKey, opt => opt.Ignore())
-            .ForMember(dest => dest.TimeStamp, opt => opt.Ignore())
-            .ForMember(dest => dest.EuropeanAqiText, opt => opt.Ignore())
-            .ForMember(dest => dest.UsAqiText, opt => opt.Ignore());
+            .ForMember(dest => dest.Gis, opt => opt.Ignore());
     }
 }
